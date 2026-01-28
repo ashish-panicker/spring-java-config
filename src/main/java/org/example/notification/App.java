2f30a98d352db4344c951c4ac0d36be1cbbabee3
ashish-panicker <ashish.s.panicker@proton.me>
2026-01-28 15:54:42 +0530

    Spring with XML config

--- src/main/java/org/example/notification/App.java
+++ src/main/java/org/example/notification/App.java
@@ -1,7 +1,15 @@
 package org.example.notification;
 
+import org.example.notification.service.NotificationService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        try(ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beans.xml")){
 
+            var service = context.getBean("notificationService", NotificationService.class);
+            service.notify("ashish.s@gmail.com", "Your account has been activated.");
+        }
     }
 }
